619ad3f0e0c11f1ca04a64bdaba0d3fcb0ed4999
Thomas Loughran <tloughran3@gmail.com>
2023-11-08 14:49:34 +0000

    added tables for flights and passengers

--- start_point/src/main/java/com/example/airline_api/models/Passenger.java
+++ start_point/src/main/java/com/example/airline_api/models/Passenger.java
@@ -1,33 +1,13 @@
 package com.example.airline_api.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "passengers")
 public class Passenger {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @Column
     private String name;
-
-    @Column
     private String email;
-
-    @ManyToMany
-    @JoinTable(
-            name = "passengers_flights",
-            joinColumns = @JoinColumn(name = "passenger_id"),
-            inverseJoinColumns = @JoinColumn(name = "flight_id")
-    )
-    @JsonIgnoreProperties({"flights"})
-
     private List<Flight> flights;
 
     public Passenger(String name, String email) {
